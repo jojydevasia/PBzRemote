119bf7526991f5e832266bed489c4ff9a3d56ca3
Jojy Devasia <jojydevasia1@gmail.com>
2016-03-07 01:49:32 +0530

    test commit

--- Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest.java
+++ Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest.java
@@ -1,15 +1,51 @@
 package com.paytm.bus;
 
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.http.auth.Credentials;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
 public class PageTest {
-public static void main(String[] args){
-	
+public static void main(String[] args)throws IOException {
+	/*URL: beta1.paytm.com
+	username: paytmbeta
+	password: P@ytW8eTA
+	Open Hangout*/
 	WebDriver driver= new FirefoxDriver();
-	driver.navigate().to("https://beta1.paytm.com");
+	driver.get("http://paytm.com");
+	driver.manage().window().maximize();
+	
+	//driver.get("https://paytmbeta:P@ytW8etA@beta1-assets.paytm.com");
+	//Runtime.getRuntime().exec("D:\\JojyDevasiaFolder\\AutoIT scripts\\HandleAuth.exe");
+
+	//driver.get("http://beta1-assets.paytm.com");
+	driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+	
+	driver.findElement(By.xpath("//a[@href='/bus-tickets']")).click();
+	System.out.println(driver.findElement(By.xpath("//div[@class='box1']")).getText());
+	driver.findElement(By.xpath("//input[@id='input_32']")).sendKeys("Bangalore");
+	driver.findElement(By.xpath("//input[@id='input_33']")).sendKeys("Calicut");
+	driver.findElement(By.xpath("//input[@id='P494915865']")).sendKeys("15 Mar 2016");
+
+	
+	
+	//ArrayList al=new ArrayList();
+	 
+	//System.out.println(driver.getWindowHandles());
+	//Alert alrt=driver.switchTo().alert();
+	//alrt.sendKeys("paytmbeta");
+	//Credentials ccr=new Credentials()
+	//alrt.authenticateUsing("paytmbeta");
+	//driver.switchTo().
+	
+	
 	}
 }
--- Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PaytmHomePage.java
+++ Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PaytmHomePage.java
@@ -1,6 +1,34 @@
 package com.paytm.bus;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+
 public class PaytmHomePage {
+	WebDriver driver;
+	
+	String homTitle=null;
+	By loginLink=By.xpath("//a[@class='login']");
+	By busTicketTab=By.xpath("//a[@href='/bus-tickets']");
+	By mobileTab=By.xpath("//a[text()='Mobile']");
+	By DTHTab=By.xpath("//a[text()='DTH']");
+	By electricityTab=By.xpath("//a[text()='Electricity']");
+	By dataCardTab=By.xpath("//a[text()='Mobile']");
+	By landLineTab=By.xpath("//a[text()='Mobile']");
+	By gasTab=By.xpath("//a[text()='Mobile']");
+	By educationTab=By.xpath("//a[text()='Mobile']");
+	By insuranceTab=By.xpath("//a[text()='Mobile']");
+	By goldLoanTab=By.xpath("//a[text()='Mobile']");
+	By metroTab=By.xpath("//a[text()='Mobile']");
+	By waterTab=By.xpath("//a[text()='Mobile']");
+	By hotelsTab=By.xpath("//a[text()='Mobile']");
+	By entertainmentTab=By.xpath("//a[text()='Mobile']");
+	By addMoneytoWalletTab=By.xpath("//a[text()='Mobile']");
+
+
+
+
+
+
+
+	
 
 }
