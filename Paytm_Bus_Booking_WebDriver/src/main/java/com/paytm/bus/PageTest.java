5c090dd6cfc957cdf240610277535c0eec846cc5
Jojy Devasia <jojydevasia1@gmail.com>
2016-04-06 20:25:15 +0530

    Commit Apr 6

--- Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest.java
+++ Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.http.auth.Credentials;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -20,89 +19,78 @@ import org.testng.Assert;
 public class PageTest {
 	static WebDriver driver= new FirefoxDriver();
 	
-public static void selectTravelDate(String day,String month,String year) throws InterruptedException{
-	WebElement trvDay=driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]"));
-	WebElement trvMonth=driver.findElement(By.xpath("//div[@class='picker__month']"));
-	WebElement trvYear=driver.findElement(By.xpath("//div[@class='picker__year']"));
-	WebElement nxtButton=driver.findElement(By.xpath("//div[@class='picker__nav--next' and @data-nav='1']"));
-	
-	driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]")).click();
-	if(trvYear.getText().contains(year)){
-		if(trvMonth.getText().contains(month)){
+	public static void selectTravelDate(String day,String monthYear) throws InterruptedException{
+				
+		String trvMonth_Year=driver.findElement(By.xpath("//div[@class='picker__header']")).getText();
+		WebElement nxtButton=driver.findElement(By.xpath("//div[contains(@class,'picker__nav--next') and @data-nav='1']"));
+
+		if(trvMonth_Year.contains(monthYear)){
 			
-			driver.findElement(By.xpath("//table//div[text()='"+day+"' and contains((@class),'infocus')]")).click();
-		} else {
+			List<WebElement> availableDays=driver.findElements(By.xpath("//table//div[contains(normalize-space(@class),'infocus') and not(@aria-disabled='true')]"));
+			for(WebElement i:availableDays){
+				System.out.println(i.getText());
+				if (i.getText().contains(day)){
+					i.click();
+					return;
+				}
+			}
+
+		}else{
 			nxtButton.click();
 			Thread.sleep(2000);
-			
+			selectTravelDate(day,monthYear);
 		}
-		
 	}
 
-}
 	
 public static void main(String[] args)throws IOException, InterruptedException {
 	
-	
 	driver.manage().window().maximize();
-	
-
+	/*//The following block is just for staging environment
 	//driver.get("https://paytmbeta:P@ytW8eTA@beta1-assets.paytm.com");
 	//Runtime.getRuntime().exec("D:\\JojyDevasiaFolder\\AutoIT scripts\\HandleAuth.exe");
-	//driver.get("https://beta1.paytm.com");
+	//driver.get("https://beta1.paytm.com");   */
+	
 	driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-	
-	//Thread.sleep(5000);
-		
-		 
-   	driver.get("http://paytm.com");
-
+	driver.get("http://paytm.com");
 	driver.findElement(By.xpath("//a[@href='/bus-tickets']")).click();
-	System.out.println(driver.findElement(By.xpath("//div[@class='box1']")).getText());
-	System.out.println("*****************************************************");
-	System.out.println(driver.findElement(By.xpath("//label[text()='From (Origin)']")).getText());
-	WebElement we=driver.findElement(By.xpath("//label[text()='From (Origin)']/following-sibling::input"));
-	System.out.println(we);
-
-	
+	//System.out.println(driver.findElement(By.xpath("//label[text()='From (Origin)']")).getText());
+	//WebElement we=driver.findElement(By.xpath("//label[text()='From (Origin)']/following-sibling::input"));
+	//System.out.println(we);
 	driver.findElement(By.xpath("//label[text()='From (Origin)']/following-sibling::input")).sendKeys("Bangalore");
 	//clicking on the container to confirm destination
 	driver.findElement(By.xpath("//ul/li[1]/md-input-container")).click();
-
 	driver.findElement(By.xpath("//label[text()='To (Destination)']/following-sibling::input")).sendKeys("Goa");
 	//clicking on the container to confirm destination
 	driver.findElement(By.xpath("//ul/li[2]/md-input-container")).click();
-	boolean enStatus=driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]")).isEnabled();
-	System.out.println("EnStatus: "+enStatus);
-	
-	String curDate1=driver.findElement(By.xpath("//button[@value='Tomorrow']")).getText();
-	
-	/*Thread.sleep(3000);
+	//boolean enStatus=driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]")).isEnabled();
+	//System.out.println("EnStatus: "+enStatus);
+	//String curDate1=driver.findElement(By.xpath("//button[@value='Tomorrow']")).getText();
+	Thread.sleep(5000);
+	/*
 	driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]")).click();
 	driver.findElement(By.xpath("//div[@class='picker__month']"));
 	driver.findElement(By.xpath("//div[@class='picker__year']"));
 	driver.findElement(By.xpath("//div[@class='picker__nav--next' and @data-nav='1']")); */
-
-	
-	selectTravelDate("12","April","2016");
-	//driver.findElement(By.xpath("//table//div[text()='30' and contains((@class),'infocus')]")).click();
-	
-	
-	
+	///clicking on calendar obj
+	driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]")).click();
+	//calling date selection method
+	selectTravelDate("4","May2016");
+	///use the following in order too select today or tomorrow.
+	//driver.findElement(By.xpath("//button[@value='Today']")).click();
 	//driver.findElement(By.xpath("//button[@value='Tomorrow']")).click();
-	
+	//to select no of seats
 	driver.findElement(By.cssSelector(".md-select-icon")).click();
-
-	System.out.println("CurDate  :"+curDate1);
-	WebElement selPaxNo=driver.findElement(By.xpath("//md-option[1]"));
-	selPaxNo.click();driver.findElement(By.xpath("//span[text()='Search Buses']")).click();  
+	WebElement selPaxNo=driver.findElement(By.xpath("//md-option[1]"));//[1] here, denotes the no of passengers
+	selPaxNo.click();
+	driver.findElement(By.xpath("//span[text()='Search Buses']")).click();  
 	
 	//Next Page>******************************
-	//driver.get("https://paytm.com/bus-tickets/search/bangalore/goa/2016-03-27/1");
+	//driver.get("https://paytm.com/bus-tickets/search/bangalore/goa/2016-04-27/1");
 	
-	System.out.println("Bus List UI\n*******************");
-	String busListMsg=driver.findElement(By.className("detaShow")).getText();
-	System.out.println(busListMsg+"\n");
+	String busListMsg=driver.findElement(By.className("detaShow")).getText(); //Result header
+	
+	/*System.out.println(busListMsg+"\n");
 	if(busListMsg.toLowerCase().contains("bangalore to goa")){
 		System.out.println("++++++++ List is for Bangalore to Goa. Go Ahead+++++++++");
 	}else{
@@ -110,31 +98,45 @@ public static void main(String[] args)throws IOException, InterruptedException {
 	}
 	//Assert.assertTrue(busListMsg.contains("Bangalore to goa"), "Wrong route in response. Test Failed");
 	String busDetails=driver.findElement(By.xpath("//div[@class='bus-ticket-container'][2]")).getText();
-	System.out.println(busDetails);
-	driver.findElement(By.xpath("//button/span[text()='Select Seats'][1]")).click();
-	//  in seat selection panel
-	List<WebElement> avlSeats=driver.findElements(By.cssSelector(".square.available"));
-	if(avlSeats.size()>=1){
-		avlSeats.get(1).click();
-	}else{
-		driver.findElement(By.xpath("//button[@ng-click='cancel()']")).click();
-		System.err.println("No Seats Available");
-	 }
+	System.out.println(busDetails); */
+	List<WebElement> busRecord=driver.findElements(By.xpath("//div[@class='bus-ticket-container']"));
 	
-	String selSeats=driver.findElement(By.xpath("//div[contains(text(),'Selected Seats')]/following-sibling::span")).getText();
-	System.out.println("Seats Selected"+selSeats);
+	for(int j=1;j<=busRecord.size();j++){
+		String seatMsg=(busRecord.get(j).findElement(By.className("seatsAvailable"))).getText();
+		String msgSplit[]=seatMsg.split(":");
+		int numSeatAvl=Integer.parseInt(msgSplit[1].trim());
+		if(numSeatAvl>0){
+			driver.findElement(By.xpath("//button/span[text()='Select Seats'][1]")).click();
+		//  in seat selection panel
+			List<WebElement> avlSeats=driver.findElements(By.xpath("//div[@class='square2 available' or @class='square available']"));
+			if(avlSeats.size()>=1){
+				avlSeats.get(1).click();
+				//printing the selected seat
+				String selSeats=driver.findElement(By.xpath("//div[contains(text(),'Selected Seats')]/following-sibling::span")).getText();
+				System.out.println("Seats Selected:  "+selSeats);
+				//To select the boarding point open the list
+				driver.findElement(By.xpath("//md-select[@ng-model='boardingPoint']")).click();
+				//print content in list
+				System.out.println(driver.findElement(By.className("md-text")).getText()); 
+				//getContents of list
+				List<WebElement> bordPoint=driver.findElements(By.className("md-text"));
+				int noBrdPoint=bordPoint.size();
+				bordPoint.get(2).click();
+				driver.findElement(By.xpath("//button/span[contains(text(),'Proceed to pay')]	")).click();
+				break;
+			}else{
+				
+				driver.findElement(By.xpath("//button[@ng-click='cancel()']")).click();
+				
+				System.err.println("No Seats Available");
+			 }
+		}
+	}
 	
-	driver.findElement(By.xpath("//md-select[@ng-model='boardingPoint']")).click();
-	
-	//System.out.println(driver.getPageSource());
 	
 	
-	//driver.findElement(By.cssSelector("//div[contains(text(),'PM') OR contains(text(),'AM')]")).click();	
-	System.out.println(driver.findElement(By.className("md-text")).getText()); 
-	List<WebElement> bordPoint=driver.findElements(By.className("md-text"));
-	int noBrdPoint=bordPoint.size();
-	bordPoint.get(2).click();
-	driver.findElement(By.xpath("//button/span[contains(text(),'Proceed to pay')]	")).click();
+	
+	
 	Thread.sleep(10000);
 	driver.switchTo().frame(1);
 	String fTitl=driver.getTitle();
--- Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest2.java
+++ Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest2.java
@@ -13,61 +13,21 @@ import org.openqa.selenium.interactions.Actions;
 public class PageTest2 {
 	static WebDriver driver= new FirefoxDriver();
 
-	public static void selectTravelDate(String day,String month,String year) throws InterruptedException{
-		//WebElement trvDay=driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]"));
-		WebElement trvMonth=driver.findElement(By.xpath("//div[@class='picker__month']"));
-		WebElement trvYear=driver.findElement(By.xpath("//div[@class='picker__year']"));
-		WebElement nxtButton=driver.findElement(By.xpath("//div[@class='picker__nav--next' and @data-nav='1']"));
-
-		if(trvYear.getText().contains(year)){
-			if(trvMonth.getText().contains(month)){
-
-				List<WebElement> bookingDays=driver.findElements(By.xpath("//table//div[contains(normalize-space(@class),'infocus') and not(@aria-disabled='true')]"));
-
-				
-				for(WebElement i:bookingDays){
-					System.out.println(i.getText());
-					if (i.getText().contains(day)){
-						i.click();
-						
-					}
-
-				}
-				
-			} else {
-				nxtButton.click();
-				Thread.sleep(2000);
-				selectTravelDate(day,month,year);
-			}
-
-
-		}
-
-	}
-
+	
 	public static void main(String[] args) throws InterruptedException{
 		driver.get("http://paytm.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		Date date= new Date();
-		System.out.println(date);
+		driver.findElement(By.xpath("//a[contains(text(),'Log In')]")).click();
+		Thread.sleep(3000);
+		//driver.switchTo().frame
+		driver.findElement(By.xpath("//md-input-container[1]/input")).sendKeys("9448879224");
+		driver.findElement(By.xpath("//md-input-container[2]/input")).sendKeys("$$@P0lleH");
+		driver.findElement(By.xpath("//button/span[normalize-space(text()='Secure Login')]")).click();
 
-
-		driver.findElement(By.xpath("//a[text()='Bus Tickets']")).click();
-		
-		Actions act= new Actions(driver);
-		act.contextClick().perform();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//label[text()='Select Date']/following-sibling::input[1]")).click();
-		try {
-			selectTravelDate("4","June","2016");
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		finally{
+		//finally{
 			//driver.quit();
-		}
+		//}
 	}
 
 }
