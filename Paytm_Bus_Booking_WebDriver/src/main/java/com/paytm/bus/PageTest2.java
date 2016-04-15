1db8ff0ed8f8fb08bc792f6468a2685005c3b388
Jojy Devasia <jojydevasia1@gmail.com>
2016-04-15 17:15:36 +0530

    commit Aug 15

--- Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest2.java
+++ Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PageTest2.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,35 +22,48 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 
 
 
+
 public class PageTest2 {
 	static WebDriver driver;
 	
 	public static void main(String[] args) throws InterruptedException, InvalidFormatException, IOException{
+		//System.setProperty("webdriver.chrome.driver", "D:\\JojyDevasiaFolder\\Automation sw Lib\\chromedriver\\chromedriver.exe");
+		String uid=ReadXL.getTestData("Login",2,0);
+		String pwd=ReadXL.getTestData("Login",2,1);
+
 		driver= new FirefoxDriver();
+		//driver=new ChromeDriver();
 		driver.get("http://paytm.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		
 		//Log In
 		driver.findElement(By.xpath("//a[contains(text(),'Log In')]")).click();//Log In link in home page.
-		Thread.sleep(3000);
+		Thread.sleep(10000);
 	    driver.switchTo().frame(2);
-		driver.findElement(By.xpath("//md-input-container[1]/input")).sendKeys("uid");
-		driver.findElement(By.xpath("//md-input-container[2]/input")).sendKeys("pwd");
-		driver.findElement(By.xpath("//button/span[normalize-space(text()='Secure Login')]")).click();
-		Thread.sleep(15000);
-		driver.findElement(By.xpath("//md-content//input[@id='input_2']")).sendKeys("OTP");///OTP input  box
-		System.out.println(driver.findElement(By.xpath("//md-content/button/span[text()='VERIFY']")).getText());
-		driver.findElement(By.xpath("//md-content/button/span[text()='VERIFY']")).click(); //button	
-		driver.findElement(By.xpath("//a[starts-with(text(),'Forgot Password')]"));//forgot password link
-		driver.findElement(By.xpath("//li/span[starts-with(text(),'We will send a link on your')]"));//forgot password UI message
-		driver.findElement(By.xpath("//label[starts-with(text(),'Enter your Mobile')]//following-sibling::input"));//mobile number field for forgot password
-		driver.findElement(By.xpath("//span[text()='CANCEL']/ancestor::a"));//cancel button in forgot pwd UI.
-		driver.findElement(By.xpath("//span[text()='RESET PASSWORD']/ancestor::button"));//Reset pwd button
-		driver.findElement(By.xpath("//md-dialog/button[@ng-click='closeModal()']"));// login frame close button
+		driver.findElement(By.xpath("//md-input-container[1]/input")).sendKeys(uid);
+		driver.findElement(By.xpath("//md-input-container[2]/input")).sendKeys(pwd);
+		//driver.findElement(By.xpath("//button/span[normalize-space(text()='Secure Login')]")).click();
+		//Thread.sleep(20000);
+		//String otp=ReadXL.getTestData("Login",2,2);
+		//Thread.sleep(20000);
+		//driver.findElement(By.xpath("//md-content//input[@id='input_2']")).sendKeys(otp);///OTP input  box
+		//System.out.println(driver.findElement(By.xpath("//md-content/button/span[text()='VERIFY']")).getText());
+		//driver.findElement(By.xpath("//md-content/button/span[text()='VERIFY']")).click(); //button	
+		driver.findElement(By.xpath("//a[starts-with(text(),'Forgot Password')]")).click();//forgot password link
+		System.out.println(driver.findElement(By.xpath("//li/span[starts-with(text(),'We will send a link on your')]")).getText());//forgot password UI message
+		Thread.sleep(3000);
+		//driver.findElement(By.xpath("//label[starts-with(text(),'Enter your Mobile')]")).sendKeys("9448879224");
+		driver.findElement(By.xpath("//label[starts-with(text(),'Enter your Mobile')]")).click();
+		driver.findElement(By.name("username")).sendKeys("9448879224");
+
+		//driver.findElement(By.xpath("//label[starts-with(text(),'Enter your Mobile')]//following-sibling::input")).sendKeys("9448879224");//mobile number field for forgot password
+		//driver.findElement(By	.xpath("//span[text()='CANCEL']/ancestor::a")).click();//cancel button in forgot pwd UI.
+		//driver.findElement(By.xpath("//span[text()='RESET PASSWORD']/ancestor::button")).click();//Reset pwd button
+		//driver.findElement(By.xpath("//md-dialog/button[@ng-click='closeModal()']"));// login frame close button
 		
 			//driver.quit();
 		
 	}
-
+	
 }
