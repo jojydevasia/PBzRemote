4b8621c945aa9fa938aedf144e22e9adf6a9c77b
Jojy Devasia <jojydevasia1@gmail.com>
2016-07-19 02:25:39 +0530

    July 19 2.30 am commit

--- Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PaytmBusPassengerDetailPage.java
+++ Paytm_Bus_Booking_WebDriver/src/main/java/com/paytm/bus/PaytmBusPassengerDetailPage.java
@@ -25,6 +25,13 @@ public class PaytmBusPassengerDetailPage {
 	By arrivalCityName=By.cssSelector("h5:contains('Arrival')+p");  //xpath( //li/h5[contains(text(),'Arrival')]/following-sibling::p )
 	By operatorName=By.cssSelector("li>div:contains('Bus Operator')+div"); //xpath( //div[text()='Bus Operator']/following-sibling::div )
 	By busTypeText=By.cssSelector("li>div:contains('Bus Type')+div"); //xpath( //li/div[text()='Bus Type']/following-sibling::div )
-		
-	
+	By boardingTimeText=By.cssSelector("li>div:contains('Boarding Time')+div"); //xpath ( //li/div[text()='Boarding Time']/following-sibling::div )
+	By seatNumberText=By.cssSelector("li>div:contains('Seats Number')+div"); // xpath ( //li/div[contains(text(),'Seat')]/following-sibling::div )
+	By boardingPointText=By.cssSelector("li>div:contains('Boarding Point')+div");  //xpath ( //li/div[text()='Boarding Point']/following-sibling::div )
+	By totalFareText=By.cssSelector("div.left+div"); //xpath( //div[contains(text(),'Total Fare')]/following-sibling::div	 )
+	By proceedToPayButton=By.cssSelector("button>span:contains('Proceed to Pay')");  //xpath ( //button/span[contains(text(),'Proceed to Pay')] )
+	By cancellationPolicyLink=By.cssSelector("a:contains('Cancellation Policy')");  //xpath ( //a[contains(text(),'Cancellation Policy')] )
+	By TnCLink=	By.cssSelector("a:contains('Terms and Conditions')"); //  xpath( //a[contains(text(),'Terms and Conditions')] )
+	By promoCodeLink=By.cssSelector("div.'promo'>a:contains('Promo Code')");  //xpath ( //div[@class='promo']/a[contains(text(),'Promo Code')] )
+	By promoCodeInputBox=By.cssSelector("input[ng-model='promoCode']"); //xpath ( //input[@ng-model='promoCode'] )
 }
